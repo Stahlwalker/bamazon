e7f7d13354f264c27cbeedc186398103c63392f0
Luke Stahl <Stahl1109@gmail.com>
2018-02-26 14:28:48 -0600

    deleted data replication

--- bamazonHW.sql
+++ bamazonHW.sql
@@ -15,16 +15,19 @@ USE bamazonDB;
 
 --   raw_total DECIMAL(10,4) NULL,
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Eloquent JavaScript", "Books", 31.96, 47),
-("iCasso Macbook Case", "Electronics", 18.99, 11),
-("Men's Weight Lifting Gloves", "Sports & Outdoors", 10.99, 15),
-("Codenames", "Toys & Games", 14.79, 32),
-("Yrd TECH Apple Watch Band", "Cell Phones & Accessories", 10.41, 53),
-("Superman No. 75", "Books", 22.99, 1),
-("Revision Skincare Vitamin C Lotion", "Beauty & Personal Care", 115.00, 50),
-("Packers Super Bowl XLV Pennant", "Sports Collectibles", 12.99, 2),
-("Odstore Love Balloon Banner", "Home & Kitchen", 4.99, 12),
-("Krylon Looking Glass Aerosol Spray", "Tools & Home Improvement", 13.47, 20);
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Eloquent JavaScript", "Books", 31.96, 47),
+-- ("iCasso Macbook Case", "Electronics", 18.99, 11),
+-- ("Men's Weight Lifting Gloves", "Sports & Outdoors", 10.99, 15),
+-- ("Codenames", "Toys & Games", 14.79, 32),
+-- ("Yrd TECH Apple Watch Band", "Cell Phones & Accessories", 10.41, 53),
+-- ("Superman No. 75", "Books", 22.99, 1),
+-- ("Revision Skincare Vitamin C Lotion", "Beauty & Personal Care", 115.00, 50),
+-- ("Packers Super Bowl XLV Pennant", "Sports Collectibles", 12.99, 2),
+-- ("Odstore Love Balloon Banner", "Home & Kitchen", 4.99, 12),
+-- ("Krylon Looking Glass Aerosol Spray", "Tools & Home Improvement", 13.47, 20);
 
 select * from products;
+
+-- DELETE FROM products WHERE id in (11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
